6a7054aeb47ec09d7d0f458246fe529005204271
Sergio Bayona <sbayona@martianinteractive.com>
2024-05-24 09:47:15 -0500

    placeholder skeleton

---
+++ spec/agentbase_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe Agentbase do
+  it "has a version number" do
+    expect(Agentbase::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
