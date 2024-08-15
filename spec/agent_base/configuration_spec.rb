76ae73242ddf432dde32e2cca4acc71e7b6c981d
Sergio Bayona <sbayona@martianinteractive.com>
2024-08-14 23:16:45 -0500

    config spec

--- spec/agent_base/configuration_spec.rb
+++ spec/agent_base/configuration_spec.rb
@@ -6,9 +6,12 @@ RSpec.describe AgentBase::Configuration do
   let(:custom_client) { double('CustomClient') }
 
   describe '#initialize' do
-    it 'sets the default values if not provided' do
+    before do
       allow(ENV).to receive(:[]).with('OPENAI_API_KEY').and_return('your_api_key')
       allow(ENV).to receive(:[]).with('OPENAI_ORGANIZATION_ID').and_return(nil)
+    end
+
+    it 'sets the default values if not provided' do
       configuration = described_class.new
       expect(configuration.client_retries).to eq(1)
       expect(configuration.client).to eq(OpenAI::Client)
@@ -17,6 +20,7 @@ RSpec.describe AgentBase::Configuration do
       expect(configuration.log_errors).to eq(true)
       expect(configuration.organization_id).to be_nil
       expect(configuration.agents_path).to eq('app/agents')
+      expect(configuration.agent_file_name).to eq('agent.rb')
     end
 
     it 'sets the custom values if provided' do
@@ -26,14 +30,18 @@ RSpec.describe AgentBase::Configuration do
         model: 'gpt-4o-turbo',
         client_retries: 5,
         log_errors: false,
-        agents_path: 'app/custom/path'
+        organization_id: 'org_123',
+        agents_path: 'app/custom/path',
+        agent_file_name: 'custom_agent.rb'
       )
       expect(configuration.client).to eq(custom_client)
       expect(configuration.api_key).to eq('abc123')
       expect(configuration.model).to eq('gpt-4o-turbo')
       expect(configuration.client_retries).to eq(5)
       expect(configuration.log_errors).to eq(false)
+      expect(configuration.organization_id).to eq('org_123')
       expect(configuration.agents_path).to eq('app/custom/path')
+      expect(configuration.agent_file_name).to eq('custom_agent.rb')
     end
 
     it 'yields self if a block is given' do
@@ -53,6 +61,7 @@ RSpec.describe AgentBase::Configuration do
         config.log_errors = false
         config.organization_id = 'org_123'
         config.agents_path = 'app/custom/path'
+        config.agent_file_name = 'custom_agent.rb'
       end
     end
 
@@ -64,19 +73,24 @@ RSpec.describe AgentBase::Configuration do
       expect(described_class.settings.log_errors).to eq(false)
       expect(described_class.settings.organization_id).to eq('org_123')
       expect(described_class.settings.agents_path).to eq('app/custom/path')
+      expect(described_class.settings.agent_file_name).to eq('custom_agent.rb')
     end
   end
 
   describe '.reset' do
     before do
       described_class.configure do |config|
-        config.client = 'custom_client'
+        config.client = custom_client
         config.model = 'custom_model'
         config.client_retries = 5
+        config.log_errors = false
+        config.organization_id = 'org_123'
+        config.agents_path = 'app/custom/path'
+        config.agent_file_name = 'custom_agent.rb'
       end
     end
 
-    it 'resets the settings' do
+    it 'resets the settings to default values' do
       described_class.reset
       expect(described_class.settings.client).to eq(OpenAI::Client)
       expect(described_class.settings.model).to eq('gpt-3.5-turbo')
@@ -84,6 +98,7 @@ RSpec.describe AgentBase::Configuration do
       expect(described_class.settings.log_errors).to eq(true)
       expect(described_class.settings.organization_id).to be_nil
       expect(described_class.settings.agents_path).to eq('app/agents')
+      expect(described_class.settings.agent_file_name).to eq('agent.rb')
     end
   end
 end
