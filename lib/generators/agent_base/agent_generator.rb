70019adca74941693d01b81724a9754b82176de8
Sergio Bayona <sbayona@martianinteractive.com>
2024-08-16 11:23:52 -0500

    check installation has run

--- lib/generators/agent_base/agent_generator.rb
+++ lib/generators/agent_base/agent_generator.rb
@@ -1,8 +1,12 @@
-# lib/generators/agent_base/agent_generator.rb
-
 module AgentBase
   class AgentGenerator < Rails::Generators::NamedBase
     source_root File.expand_path('templates', __dir__)
+
+    def check_installation
+      return if File.exist?(Rails.root.join('config', 'agents.rb'))
+
+      raise Thor::Error, "AgentBase installation not found. Please run 'rails generate agent_base:install' first."
+    end
 
     def create_agent_file
       template 'agent.rb', File.join('app/agents', class_path, "#{file_name}.rb")
